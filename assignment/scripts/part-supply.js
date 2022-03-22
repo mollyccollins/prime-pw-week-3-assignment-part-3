e0a2854e00da35f3342847a6a6b0b72acc4b83af
Molly Collins <molly.c.collins@gmail.com>
2022-03-22 17:17:09 -0500

    parts supply assign

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -3,21 +3,30 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+
+let partsNeeded = 40
+console.log('1. Number of partsNeeded:' , partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+
+let supplyChanges = [ 3, 5, -6, 0, 7, 11];
+console.log('2. Array of supplyChanges:', supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+
+console.log('3. Second supplyChanges is:', supplyChanges[2]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+
+supplyChanges.pop()
+console.log('4. Removed item:', supplyChanges.length);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Adding 25 to supplyChanges.');
+
+supplyChanges.push(25);
+console.log('5. Adding 25 to supplyChanges.'), supplyChanges [supplyChanges- 1];
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -26,6 +35,22 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 
+let max = 7
+for( let i=0; i<max; i++ ) {
+    console.log( 'in for the loop. i', i ); 
+}
+    for( let i=0; i < supplyChanges [i]; i++);{
+
+
+        if( supplyChanges >= 0);
+        console.log( 'Added x parts.')
+    }
+        else if( supplyChanges == 0);{
+        console.log( 'No change.')
+        {
+        if( supplyChanges=0);
+        console.log( 'Removed x parts.')
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
